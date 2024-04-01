007dcc28dd1b79a7743a1076c7351c3cae88ab46
Quinn <quinn.mohamed12@gmail.com>
2024-03-31 21:32:26 -0400

    edited init script so that passwords are encrypted

--- utilities/initialize_quinn.js
+++ utilities/initialize_quinn.js
@@ -353,11 +353,11 @@ async function loadDummySemesterCourses(courseData) {
     // await loadGroups(GroupsJSON);
     // await loadCourseGroups(CourseGroupsJSON);
     // await loadPrerequisites(PrerequisitesJSON);
-    // await loadDummyStudents(StudentsJSON);
-    await loadDummySemesters(SemestersJSON);
+    await loadDummyStudents(StudentsJSON);
+    // await loadDummySemesters(SemestersJSON);
     await loadDummyStudentCourses(StudentCoursesJSON);
-    // await loadDummyPrereq_Coursegrp(CoursesJSON);
-    // await loadDummyAntireq(CoursesJSON);
-    // await loadDummySemesterCourses(CoursesJSON);
+    await loadDummyPrereq_Coursegrp(CoursesJSON);
+    await loadDummyAntireq(CoursesJSON);
+    await loadDummySemesterCourses(CoursesJSON);
     console.log('Done');
 })()
