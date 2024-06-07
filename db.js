6cc3bf0e1ee291f1cd833fa73940a071517f0246
Nicholas <nicosmith.smith3@gmail.com>
2024-06-07 17:11:30 +0000

    Updated: to fix route for semester configuration page on frontend

--- db.js
+++ db.js
@@ -9,28 +9,28 @@ const Sequelize = require("sequelize");
 
 //SQLITE DATABASE
 // SQLite database configuration
-const db = new Sequelize({
-  dialect: 'sqlite',
-  logging: false,
-  storage: 'database.sqlite', // Replace with the path to your SQLite database file
-  pool: {
-    max: 50, // Maximum number of connections in the pool
-    min: 0,  // Minimum number of connections in the pool
-    acquire: 30000, // Maximum time, in milliseconds, that a connection can be idle before being released
-    idle: 10000  // Maximum time, in milliseconds, that a connection can be idle before being closed
-  },
-  dialectOptions: {
-    // Enable WAL mode
-    // https://sqldocs.org/sqlite/sqlite-write-ahead-logging/#when-to-use-wal-mode
-    mode: Sequelize.QueryTypes.WAL
-  }
-});
+// const db = new Sequelize({
+//   dialect: 'sqlite',
+//   logging: false,
+//   storage: 'database.sqlite', // Replace with the path to your SQLite database file
+//   pool: {
+//     max: 50, // Maximum number of connections in the pool
+//     min: 0,  // Minimum number of connections in the pool
+//     acquire: 30000, // Maximum time, in milliseconds, that a connection can be idle before being released
+//     idle: 10000  // Maximum time, in milliseconds, that a connection can be idle before being closed
+//   },
+//   dialectOptions: {
+//     // Enable WAL mode
+//     // https://sqldocs.org/sqlite/sqlite-write-ahead-logging/#when-to-use-wal-mode
+//     mode: Sequelize.QueryTypes.WAL
+//   }
+// });
 
-// Increase the busy timeout
-db.query('PRAGMA busy_timeout = 30000;'); // Set busy timeout to 30 seconds
+// // Increase the busy timeout
+// db.query('PRAGMA busy_timeout = 30000;'); // Set busy timeout to 30 seconds
 
-// Enable WAL mode
-db.query('PRAGMA journal_mode = WAL;');
+// // Enable WAL mode
+// db.query('PRAGMA journal_mode = WAL;');
 
 
 // const db = new Sequelize({
@@ -50,19 +50,19 @@ db.query('PRAGMA journal_mode = WAL;');
 // });
 
 
-// const db = new Sequelize({
-//   dialect: "postgres",
-//   host: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS@dpg-ci1kmm0rddl1m6hknfbg-a.oregon-postgres.render.com/myadvisor_database",
-//   port: "5432",
-//   database: "myadvisor_database",
-//   username: "myadvisor_database_user",
-//   password: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS",
-//   pool: {
-//     max: 3,
-//     min: 0,
-//     idle: 10000,
-//   },
-// });
+const db = new Sequelize({
+  dialect: "postgres",
+  host: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS@dpg-ci1kmm0rddl1m6hknfbg-a.oregon-postgres.render.com/myadvisor_database",
+  port: "5432",
+  database: "myadvisor_database",
+  username: "myadvisor_database_user",
+  password: "YuKpP0lz6KhxNnsRLExqDQrl64bJj6OS",
+  pool: {
+    max: 3,
+    min: 0,
+    idle: 10000,
+  },
+});
 
 // // tests database connection on server startup to see if the connection is OK.
 // db.authenticate()
