d02ada482cf4462a3c117f7142fe5bf1e5d30f4f
firepenguindisopanda <nicosmith.smith3@gmail.com>
2024-06-15 14:45:36 -0400

    Updated: with eager loading using include, reducs the number of database queries made from previous code, improved readability and scalability

--- controllers/getStudentCourses.js
+++ controllers/getStudentCourses.js
@@ -2,50 +2,43 @@ const StudentCourses = require("../models/StudentCourse");
 const Semester = require("../models/Semester");
 const Course = require("../models/Course");
 
-async function getStudentsCourses(studentId){//returns list of json
-    const student_courses = await StudentCourses.findAll({ 
-        where: { 
-            studentId: studentId
-        } 
-    });
-    const processedCourses = await Promise.all(student_courses.map(async (info) => {
-        const std_course_info = info.dataValues;
-
-        const [sem_info, course_info] = await Promise.all([
-            Semester.findOne({
-                attributes: ['academicYear','num'],
-                where :{
-                    id : std_course_info['semesterId']
+async function getStudentsCourses(studentId) {
+    try {
+        const studentCourses = await StudentCourses.findAll({
+            where: { studentId: studentId },
+            include: [
+                {
+                    model: Course,
+                    attributes: ['title', 'credits']
+                },
+                {
+                    model: Semester,
+                    attributes: ['academicYear', 'num']
                 }
-            }).then(async(sem)=>{
-                return [sem.get('academicYear'),sem.get('num')];
-            }),
-            Course.findOne({
-                attributes : ['title','credits'],
-                where: {
-                    code: std_course_info['courseCode']
-                }
-            }).then(async(course)=>{
-                return [course.get('title'),course.get('credits')];
-            })
-        ]);
+            ]
+        });
 
-        std_course_info['yearDone'] = sem_info[0];
-        std_course_info['semesterDone'] = sem_info[1];
-        std_course_info['courseName'] = course_info[0];
-        std_course_info['courseCredits'] = course_info[1];
-        return {
-            id : std_course_info.id,
-            courseCode: std_course_info.courseCode,
-            courseName: course_info[0],
-            creditHours: course_info[1],
-            grade : std_course_info.grade,
-            semester: sem_info[1],
-            academicYear: sem_info[0]
-        };
-    }));
+        const processedCourses = studentCourses.map(info => {
+            const std_course_info = info.dataValues;
+            const course_info = info.Course.dataValues;
+            const sem_info = info.Semester.dataValues;
 
-    return processedCourses;
-    
+            return {
+                id: std_course_info.id,
+                courseCode: std_course_info.courseCode,
+                courseName: course_info.title,
+                creditHours: course_info.credits,
+                grade: std_course_info.grade,
+                semester: sem_info.num,
+                academicYear: sem_info.academicYear
+            };
+        });
+
+        return processedCourses;
+    } catch (error) {
+        console.error("Error fetching student courses:", error);
+        throw error;
+    }
 }
+
 module.exports = { getStudentsCourses }
