17906aef05b54b3c9e65238ac462fb7accaaae14
Paul Taylor <123190211+uni-paul-taylor2@users.noreply.github.com>
2024-08-22 15:09:50 +0000

    added pagination todo: - remove static initialisation files from db and garbage from scripts - populate test script with actual tests

--- routes/courses.js
+++ routes/courses.js
@@ -9,13 +9,15 @@ const Prerequisites = require("../models/Prerequisite");
 const Programme = require("../models/Programme");
 const ProgrammeCourse = require("../models/ProgrammeCourse");
 
+const {paginate} = require('../scripts/routeUtils'); //import pagination function
+
 // get all courses in the database
 router.get("/all", async (req, res) => {
 
     try {
         // finds all the courses and responds with a json list 
         const courses = await Course.findAll();
-        res.status(200).json(courses);
+        res.status(200).json(paginate(courses,req));
     }
     catch (err) {
         console.log("Error: ", err.message);
@@ -32,7 +34,7 @@ router.get("/departments" , async (req, res) => {
             attributes: ['department'],
             group: ['department']
         });
-        res.status(200).json(departments);
+        res.status(200).json(paginate(departments,req));
     }
     catch (err) {
         console.log("Error: ", err.message);
--- scripts/routeUtils.js
+++ scripts/routeUtils.js
@@ -85,6 +85,15 @@ const studentAccountVerification = async (req, res, next) => {
     }
 }
 
+function paginate(data,request){
+    const page_number = Number(request.headers['page_number']||undefined)
+    const page_size = Number(request.headers['page_size']||undefined)
+    const starting = page_size * page_number;
+    if(!isNaN(page_number) && !isNaN(page_size))
+        return data.slice(starting,starting+page_size);
+    return data;
+}
+
 module.exports = {
     AppError,
     NotFoundError,
@@ -93,6 +102,7 @@ module.exports = {
     ForbiddenError,
     ConflictError,
     errorHandler,
+    paginate,
     staffAccountVerification,
     studentAccountVerification
 };
