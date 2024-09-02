d3a8366147f04a2e4efa23a4515d236125e659bf
firepenguindisopanda <nicosmith.smith3@gmail.com>
2024-09-02 09:55:08 -0400

    Updated: cors origin to fix the issue with cors not accepting requests from deployed frontend

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,10 +2,10 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
-            callback(null, true)
-        }else{
-            callback(new Error('Not allowed by CORS'))
+        if (origin && origin.startsWith(allowedOrigins)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
     },
     credentials: true,
